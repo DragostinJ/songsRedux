c195a6c383abbd7174c1a51dbfb744921c437e96
unknown <dragostinj@gmail.com>
2019-01-21 13:07:59 +0200

    Updates

---
+++ precache-manifest.40669dced2f3743df5e47b72b8c6e819.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ff2ccddacb8a6e70e7b",
+    "url": "/songList/static/js/runtime~main.5ff2ccdd.js"
+  },
+  {
+    "revision": "302582f3f69ed943f5f6",
+    "url": "/songList/static/js/main.302582f3.chunk.js"
+  },
+  {
+    "revision": "eb5a6a5a6336a6e05669",
+    "url": "/songList/static/js/1.eb5a6a5a.chunk.js"
+  },
+  {
+    "revision": "5fceb6ac0238b77c99b39da2c57ad371",
+    "url": "/songList/index.html"
+  }
+];
